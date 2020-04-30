b1581278b0f03ae9149185f749bfc9fadd488767
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-04-30 16:20:39 +0530

    final chenges

--- styles/master.js
+++ styles/master.js
@@ -1,27 +1,32 @@
 
 let square = document.querySelectorAll('.squareBox')
 let audio = document.querySelectorAll('audio')
+let icon = document.querySelectorAll('.icon')
 let play = (index) => {
 	if(index == 1 || index == 4 || index == 7 || index == 10 ){
-		audio[index].loop = true 
-		console.log(audio[index])
-		// audio[index].load()
-	}
+		audio[index].loop = true
+		let iconIndex = (index == 1) ? 0 : (index == 4?1:(index == 7?2:(index==10)?3:null) )
+		icon[iconIndex].classList.add('rotate')
+	} 
 	audio[index].currentTime = 0
 	audio[index].play()
+
+	audio[index].onended = () => {
+		square[index].classList.toggle('shadow')
+	}
+	
 }
 let pause = (index) => {
-	console.log("pause the song")
 	audio[index].loop = false
-	// console.log(tune.loop)
-	// tune.load()
 	audio[index].pause();
+	let iconIndex = (index == 1) ? 0 : (index == 4?1:(index == 7?2:(index==10)?3:null) )
+	icon[iconIndex].classList.remove('rotate')
+}
+let audioPlaying = (index) => {
+	return (audio[index].duration > 0 && !audio[index].paused)
 }
 let run = (index) => {
 	index = index - 1
-
-
-	console.log(audio[index])
 
 	square[index].classList.toggle('shadow')
 	if (square[index].className == "squareBox shadow"){
@@ -29,7 +34,4 @@ let run = (index) => {
 	} else {
 		pause(index)
 	}
-	console.log(index)
-
 }
-
