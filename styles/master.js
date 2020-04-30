2d58e171dbe23ea5887bd405967c17780c71c869
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-04-30 14:36:15 +0530

    Loop Error Solved

--- styles/master.js
+++ styles/master.js
@@ -1,49 +1,35 @@
-let audio = [
-	"sounds/pad1_sunset.mp3",
-	"sounds/pad2_sunset.mp3",
-	"sounds/pad3_sunset.mp3",
-	"sounds/pad4_sunset.mp3",
-	"sounds/pad5_sunset.mp3",
-	"sounds/pad6_sunset.mp3",
-	"sounds/pad7_sunset.mp3",
-	"sounds/pad8_sunset.mp3",
-	"sounds/pad9_sunset.mp3",
-	"sounds/pad10_sunset.mp3",
-	"sounds/pad11_sunset.mp3",
-	"sounds/pad12_sunset.mp3",
-]
 
 let square = document.querySelectorAll('.squareBox')
-
+let audio = document.querySelectorAll('audio')
+let play = (index) => {
+	if(index == 1 || index == 4 || index == 7 || index == 10 ){
+		audio[index].loop = true 
+		console.log(audio[index])
+		// audio[index].load()
+	}
+	audio[index].currentTime = 0
+	audio[index].play()
+}
+let pause = (index) => {
+	console.log("pause the song")
+	audio[index].loop = false
+	// console.log(tune.loop)
+	// tune.load()
+	audio[index].pause();
+}
 let run = (index) => {
-
 	index = index - 1
 
-	let tune = new Audio(audio[index])
-	
-	let play = (index) => {
-		if(index == 1 || index == 4 || index == 7 || index == 10 ){
-			tune.loop = true 
-			tune.load()
-		}
-		tune.play()
-	}
-	
-	let pause = (index) => {
-		console.log("pause the song")
-		tune.loop = false
-		console.log(tune.loop)
-		// tune.load()
-		tune.pause();
-	}
-	
+
+	console.log(audio[index])
+
 	square[index].classList.toggle('shadow')
-	console.log(square[index].className)
 	if (square[index].className == "squareBox shadow"){
 		play(index)
 	} else {
 		pause(index)
 	}
 	console.log(index)
+
 }
 
